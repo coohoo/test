8ffe1902e992a457f24054911ce5d8818079ebdd
coohoo <coomails@qq.com>
2015-11-03 13:39:58 +0800

    first

---
+++ Recorder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Recorder
+//
+//  Created by Devond on 15/9/23.
+//  Copyright (c) 2015年 slin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Recorder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Recorder
+//
+//  Created by Devond on 15/9/23.
+//  Copyright (c) 2015年 slin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
